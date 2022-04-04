09a1e27647bbe7151f1dfe3e8917a014c0ce6722
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-04-04 23:14:35 +0530

    [Nikhil Kumar]: DailyWage Calculated

--- EmployeeWage/EmployeeWage.py
+++ EmployeeWage/EmployeeWage.py
@@ -1,8 +1,16 @@
 import random
 
+wage_per_hr = 20
+full_day_hr = 8
 is_present = 1
 attendance = random.randint(0, 1)
 if attendance == is_present:
     print("Employee is present")
+    empHrs = full_day_hr
 else:
     print("Employee is absent")
+    empHrs = 0
+
+daily_wage = wage_per_hr * empHrs
+print("daily_wage = ", daily_wage)
+
