5032c8d3b1043aed62af7ee354d183ae569d6103
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-04-05 01:33:45 +0530

    [Nikhil Kumar]: Refactor the code using function

--- EmployeeWage/EmployeeWage.py
+++ EmployeeWage/EmployeeWage.py
@@ -1,27 +1,35 @@
 import random
 
-wage_per_hr = 20
-full_day_hr = 8
-part_time_hr = 4
-emp_present = 1
-emp_part_time = 2
-tot_working_days = 20
-tot_working_hrs = 100
-empHrs = 0
-days = 0
-while days <= tot_working_days or empHrs <= tot_working_hrs:
-    attendance = random.randint(0, 2)
-    if attendance == emp_present:
-        print("Employee is Working Full Time")
-        empHrs += full_day_hr
-    elif attendance == emp_part_time:
-        print(" Employee is Working Part Time ")
-        empHrs += part_time_hr
-    else:
-        print("Employee is Absent")
-        empHrs += 0
-    days += 1
 
-total_emp_wage = wage_per_hr * empHrs
-print("Total Employee Wage = ", total_emp_wage)
+def emp_wage():
+    wage_per_hr = 20
+    full_day_hr = 8
+    part_time_hr = 4
+    emp_present = 1
+    emp_part_time = 2
+    tot_working_days = 20
+    tot_working_hrs = 100
+    emphrs = 0
+    days = 0
+    while days <= tot_working_days or emphrs <= tot_working_hrs:
+        attendance = random.randint(0, 2)
+        if attendance == emp_present:
+            print("Employee is Working Full Time")
+            emphrs += full_day_hr
+        elif attendance == emp_part_time:
+            print(" Employee is Working Part Time ")
+            emphrs += part_time_hr
+        else:
+            print("Employee is Absent")
+            emphrs += 0
+        days += 1
+
+    total_emp_wage = wage_per_hr * emphrs
+    return "Total Employee Wage = {}".format(total_emp_wage)
+
+
+if __name__ == "__main__":
+    wage = emp_wage()
+    print(wage)
+
 
