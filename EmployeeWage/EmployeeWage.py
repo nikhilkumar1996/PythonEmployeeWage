9409a71f09a4cae69a88a86d8b02c47039042d33
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-04-05 01:23:22 +0530

    [Nikhil Kumar]: Calculate Wage for Condition

--- EmployeeWage/EmployeeWage.py
+++ EmployeeWage/EmployeeWage.py
@@ -6,8 +6,10 @@ part_time_hr = 4
 emp_present = 1
 emp_part_time = 2
 tot_working_days = 20
+tot_working_hrs = 100
 empHrs = 0
-for days in range(tot_working_days - 1):
+days = 0
+while days <= tot_working_days or empHrs <= tot_working_hrs:
     attendance = random.randint(0, 2)
     if attendance == emp_present:
         print("Employee is Working Full Time")
@@ -18,6 +20,7 @@ for days in range(tot_working_days - 1):
     else:
         print("Employee is Absent")
         empHrs += 0
+    days += 1
 
 total_emp_wage = wage_per_hr * empHrs
 print("Total Employee Wage = ", total_emp_wage)
