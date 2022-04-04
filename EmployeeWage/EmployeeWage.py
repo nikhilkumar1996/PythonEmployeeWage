bc0ed426927cc1b8e89c794c86b09a800c5e7986
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-04-04 23:46:24 +0530

    [Nikhil Kumar]:Calculated Partime Employee Wage

--- EmployeeWage/EmployeeWage.py
+++ EmployeeWage/EmployeeWage.py
@@ -2,15 +2,19 @@ import random
 
 wage_per_hr = 20
 full_day_hr = 8
-is_present = 1
-attendance = random.randint(0, 1)
-if attendance == is_present:
-    print("Employee is present")
+part_time_hr = 4
+emp_present = 1
+emp_part_time = 2
+attendance = random.randint(0, 2)
+if attendance == emp_present:
+    print("Employee is Working Full Time")
     empHrs = full_day_hr
+elif attendance == emp_part_time:
+    print("Employee is Working Part Time")
+    empHrs = part_time_hr
 else:
-    print("Employee is absent")
+    print("Employee is Absent")
     empHrs = 0
-
 daily_wage = wage_per_hr * empHrs
 print("daily_wage = ", daily_wage)
 
